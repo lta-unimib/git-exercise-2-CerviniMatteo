047829e12479f061da56d3d9f74fd6a3ce42e3ea
Teo <m.cervini1@campus.unimib.it>
2024-10-22 15:43:59 +0200

    corretto refuso riga package

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -1,8 +1,8 @@
-cpackage gitExercise2.main;
+package gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Nuova stampa");
+		System.out.println("Questa è una nuovissima stampa, che serve per simulare la modifica simultanea di una riga di codice!")
 	}
 }
