58892b3ea1db744500a825790b6d79d32695d891
Teo <m.cervini1@campus.unimib.it>
2024-10-22 15:34:54 +0200

    Sono il primo developer e ho risolto un conflitto

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -1,8 +1,8 @@
-package gitExercise2.main;
+cpackage gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Questa è una nuovissima stampa, che serve per simulare la modifica simultanea di una riga di codice!")
+		System.out.println("Nuova stampa");
 	}
 }
